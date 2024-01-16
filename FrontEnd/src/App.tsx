df9ede5861a457e031a67dea43502db5969fadf7
Adlai Gordon <adlai.gordon@mongodb.com>
2024-01-16 14:54:18 -0500

    More built out page with grid for four components

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,20 +1,39 @@
 import './App.css'
 // import { Container, TextInput, Button, Title, Box, Space } from '@mantine/core';
-import { MantineProvider } from '@mantine/core';
-import TextInputExample from './TextInputExample';
-import TextAreaExample from './TextAreaExample';
+import { MantineProvider, Container, Group, Text, Grid} from '@mantine/core';
+import '@mantine/core/styles.css';
+// import DoctorNotes from './DoctorNotes';
+
+// import TextInputExample from './TextInputExample';
+// import TextAreaExample from './TextAreaExample';
+
 // import React from 'react';
 
 function App() {
   return (
     <MantineProvider>
       <div>
-        <h1>sa hackathon</h1>
-        <div style={{ padding: '20px' }}>
+        <div className='header'>
+          <h1>DocuCare</h1>
+          <h2>Enhance the doctor-patient relationship through intelligent data utilization</h2>
+        </div>
+        {/* <div style={{ padding: '20px' }}>
           <TextInputExample />
         </div>
-        <TextAreaExample />
+        <TextAreaExample /> */}
+
+        <Grid>
+          <Grid.Col span={6}>PatientInfo </Grid.Col>
+          <Grid.Col span={6}>DoctorNotes</Grid.Col>
+          <Grid.Col span={6}>PatientChart</Grid.Col>
+          <Grid.Col span={6}>PatientAnalysis</Grid.Col>
+        </Grid>
+
+        <footer className="footer">
+          Built with MongoDB
+        </footer>
       </div>
+      
     </MantineProvider>
   );
 }
