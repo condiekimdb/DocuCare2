420b39b83455f35c39a32420f8ef2671369a0493
yberrada <younesberrada1@gmail.com>
2024-01-17 12:49:47 +0100

    edits to gateway simulator

--- backend/iot/generate.py
+++ backend/iot/generate.py
@@ -9,6 +9,7 @@ from confluent_kafka import Producer
 from bson import BSON
 import pymongo
 from datetime import datetime, timezone
+import random
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -28,8 +29,14 @@ def generate_reading(num):
     readings = ""
     # for i in range(0, num):
         # readings[i] = {
+    patients = ["efaf74f9-3de3-45dd-a5d5-26d08e8a3190", "aaa4c718-2f48-4c13-9ad0-d287cf280824",
+                "a1851c06-804e-4f31-9d8f-388cd52d4ad0","48074b70-4db4-4ab0-b9e8-361bd2ba6216",
+                "a4a67345-d7e5-4246-8e06-ed043a9764e2","90ab3acb-46b7-40d2-b66c-a04022cee236",
+                "0bcc9845-c873-492e-96e1-9771ebcbc2df","0b763e21-2a8f-4f04-86d2-f7e8f9cebb21",
+                "6590902c-4450-488c-8760-fa57d0b7422f","2ff59946-e6d0-492e-8704-a98296eedd4c"]
     readings = {
-            "metadata": { "sensorId": "Device-" + str(randint(1, 100000)), "type": "apple" },
+            # "metadata": { "sensorId": "Device-" + str(randint(1, 100000)), "type": "apple" },
+            "metadata": { "sensorId": random.choice(patients), "type": "apple" },
             # "timestamp":faker.date(),
             "timestamp": str(datetime.utcnow()),
             "gps_location": { "type": "Point", "coordinates": [ float(faker.latitude()), float( faker.longitude())]},
@@ -62,8 +69,11 @@ def delivery_callback(err, msg):
 
 
 if __name__ == "__main__":
-    num = 10
+    num = 20
     batch_size = 1
     producer = Producer(conf)
-    produce_readings(producer,num, batch_size)
+    while True:
+        produce_readings(producer,num, batch_size)
+        logging.debug("Gateway sleeping for 20 seconds...")
+        time.sleep(20)
 
